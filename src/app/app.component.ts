717b8c91878ee69d0f887fdbe97543f465335888
1237Dump <lnarayanan.1237@gmail.com>
2021-08-24 22:07:06 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  
+  constructor(private dataService: DataService){
+    
+    }
+ increaseCount(){
+   this.dataService.count++;
+ }  
+
+}
---
+++ src/app/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { DataService } from './services/data.service';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{name}}!{{dataService.count}}</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
+  providers: [DataService]
+})
+export class HelloComponent  {
+  @Input() name: string;
+
+  constructor(private dataService: DataService){
+
+  }
+}
